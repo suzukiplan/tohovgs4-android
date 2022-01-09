90551a5ea3ff55f01a2d881c57f0d217beeb5b1a
suzukiplan <suzukiplan.ys@gmail.com>
2022-01-09 15:57:13 +0900

    砂の嵐を描画（テスト）

--- app/src/main/java/com/suzukiplan/tohovgs/RetroFragment.kt
+++ app/src/main/java/com/suzukiplan/tohovgs/RetroFragment.kt
@@ -1,17 +1,28 @@
 package com.suzukiplan.tohovgs
 
+import android.graphics.Bitmap
+import android.graphics.Paint
+import android.graphics.Rect
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import com.suzukiplan.tohovgs.api.Logger
+import kotlin.random.Random
 
-class RetroFragment : Fragment() {
+class RetroFragment : Fragment(), SurfaceHolder.Callback {
     companion object {
         fun create(): RetroFragment = RetroFragment()
     }
 
-    private lateinit var surfaceView: View
+    private lateinit var surfaceView: SurfaceView
+    private lateinit var holder: SurfaceHolder
+    private lateinit var vram: Bitmap
+    private val vramRect = Rect(0, 0, 240, 320)
+    private val surfaceRect = Rect()
+    private val paint = Paint()
+    private var renderThread: Thread? = null
+    private var alive = false
+    private val random = Random(System.currentTimeMillis())
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +30,80 @@ class RetroFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
+        vram = Bitmap.createBitmap(vramRect.width(), vramRect.height(), Bitmap.Config.RGB_565)
         val view = inflater.inflate(R.layout.fragment_retro, container, false)
         surfaceView = view.findViewById(R.id.surface_view)
+        holder = surfaceView.holder
+        holder.addCallback(this)
         return view
+    }
+
+    override fun onDestroyView() {
+        vram.recycle()
+        super.onDestroyView()
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        Logger.d("surfaceCreated")
+        stopRenderThread()
+        startRenderThread()
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, f: Int, w: Int, h: Int) {
+        Logger.d("surfaceChanged: width=$w, height=$h, format=$f")
+        surfaceRect.set(0, 0, w, h)
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+        Logger.d("surfaceDestroyed")
+        stopRenderThread()
+    }
+
+    private fun startRenderThread() {
+        alive = true
+        renderThread = Thread { renderThreadMain() }
+        renderThread?.start()
+    }
+
+    private fun stopRenderThread() {
+        alive = false
+        renderThread?.join()
+        renderThread = null
+    }
+
+    private fun renderThreadMain() {
+        Logger.d("Start render thread")
+        var start: Long
+        var procTime: Long
+        val intervals = Array(3) {
+            when (it) {
+                0, 1 -> 17L
+                else -> 16L
+            }
+        }
+        var currentInterval = 0
+        paint.isAntiAlias = false
+        while (alive) {
+            start = System.currentTimeMillis()
+            tick()
+            procTime = System.currentTimeMillis() - start
+            currentInterval++
+            currentInterval %= intervals.size
+            if (procTime < 16) {
+                Thread.sleep(intervals[currentInterval] - procTime)
+            }
+        }
+        Logger.d("End render thread")
+    }
+
+    private fun tick() {
+        vram.setPixel(
+            random.nextInt(vramRect.width()),
+            random.nextInt(vramRect.height()),
+            random.nextInt(65536)
+        )
+        val canvas = holder.lockHardwareCanvas() ?: return
+        canvas.drawBitmap(vram, vramRect, surfaceRect, paint)
+        holder.unlockCanvasAndPost(canvas)
     }
 }
